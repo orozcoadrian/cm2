18642973099d5cc77faaf4234219d0dbb287bcc8
adrian orozco <orozcoadrian@gmail.com>
2014-06-21 15:02:25 -0400

    .

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,7 +2,7 @@ set (CTEST_PROJECT_NAME "Tutorial-ajo1")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "open.cdash.org")
-set(CTEST_DROP_LOCATION "submit.php?project=Tutorial-ajo1")
+set(CTEST_DROP_LOCATION "/submit.php?project=Tutorial-ajo1")
 set(CTEST_DROP_SITE_CDASH TRUE)
 set(CTEST_CDASH_VERSION "1.6")
 set(CTEST_CDASH_QUERY_VERSION TRUE)
