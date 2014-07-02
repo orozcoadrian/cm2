2e5097525395f1589798b676507b60decacb9372
adrian orozco <orozcoadrian@gmail.com>
2014-07-01 20:22:37 -0400

    .

---
+++ one/one.cxx
@@ -0,0 +1,9 @@
+// A simple program that computes the square root of a number
+#include <iostream>
+ 
+int main (int argc, char *argv[])
+{
+  auto i = 42;  
+  std::cout << "hello";
+  return 0;
+}
